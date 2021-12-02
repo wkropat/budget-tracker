a64c958c155d57677a36aa0447d182434edb5ad9
wkropat <wkropat@gmail.com>
2021-12-01 22:01:19 -0800

    added service worker and dotenv

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
+require('dotenv').config();
+const PORT = process.env.PORT ||  3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
